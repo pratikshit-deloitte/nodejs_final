039a8d0a1c4f1c9464bdd2ab458d7448ae2d20f1
Vashistha_ Pratikshit (pvashistha@deloitte.com) <pvashistha@your.hostname.com>
2023-07-23 23:21:04 +0530

    Routes

---
+++ routes/hackathon.js
@@ -0,0 +1,20 @@
+// routes/hackathonRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const hackathonController = require('../controllers/hackathonController');
+const authenticateToken = require('../middleware/auth');
+
+// List all Active/Past/Upcoming Hackathons with pagination
+router.get('/hackathons', authenticateToken, hackathonController.listHackathons);
+
+// Search Hackathons by Name, Company, Technology Stack
+router.get('/hackathons/search', authenticateToken, hackathonController.searchHackathons);
+
+// Get Hackathon details by ID
+router.get('/hackathons/:hackathonId', authenticateToken, hackathonController.getHackathonById);
+
+// List all hackathons participated by an employee
+router.get('/employees/:employeeId/hackathons', authenticateToken, hackathonController.listParticipatedHackathons);
+
+module.exports = router;
---
+++ routes/organizer.js
@@ -0,0 +1,30 @@
+// routes/organizerRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const employeeController = require('../controllers/employeeController');
+const organizerController = require('../controllers/organizerController');
+const authenticateToken = require('../middleware/auth');
+
+// Register as an Organizer (No JWT token verification needed for registration)
+router.post('/register', employeeController.registerOrganizer);
+
+// Login as an Organizer (No JWT token verification needed for login)
+router.post('/login', employeeController.loginOrganizer);
+
+// Host a Hackathon (Requires JWT token verification for organizers)
+router.post('/host', authenticateToken, organizerController.hostHackathon);
+
+// Update a Hackathon (Requires JWT token verification for organizers)
+router.put('/hackathons/:hackathonId', authenticateToken, organizerController.updateHackathon);
+
+// Delete a Hackathon (Requires JWT token verification for organizers)
+router.delete('/hackathons/:hackathonId', authenticateToken, organizerController.deleteHackathon);
+
+// List all hosted Hackathons by the Organizer
+router.get('/hackathons', authenticateToken, organizerController.listHostedHackathons);
+
+// List all participants of a Hackathon hosted by the Organizer
+router.get('/hackathons/:hackathonId/participants', authenticateToken, organizerController.listParticipants);
+
+module.exports = router;
